b4a0964bce64b6fb98a1393fa6fa731e16511862
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-29 22:03:30 +0530

    Added HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, Welcome To My Home Page</h1>
+        <p>
+            Welcome to my blog, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores dolore
+            eaque eveniet id itaque laborum non sit suscipit, unde! Accusantium adipisci delectus eius id ipsum omnis
+            rerum tenetur? Adipisci?
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias doloribus enim nam perferendis, praesentium
+            qui repellendus tenetur velit? Dolorum laborum molestiae recusandae repudiandae sapiente? Architecto
+            laboriosam mollitia vero. Quam, quos!</p>
+    </>
+)
+
+export default HomePage;
