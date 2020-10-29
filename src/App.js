030d8bfe00f652b6bcf07e67860478f6e3523570
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-29 22:08:54 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <HomePage />
-        </div>
+        <Router>
+            <div className="App">
+               <Route path="/" exact component={HomePage} />
+            </div>
+        </Router>
     );
 }
 
