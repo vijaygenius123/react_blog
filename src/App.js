6d4c052e558298559b7367615f2629a7b8df3a81
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-30 21:53:03 +0530

    Added Routes For /about /articles-list & /article

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import AboutPage from "./pages/AboutPage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
     return (
         <Router>
             <div className="App">
-               <Route path="/" exact component={HomePage} />
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/about" component={AboutPage}/>
+                <Route path="/articles-list" component={ArticlesList}/>
+                <Route path="/article" component={ArticlePage}/>
             </div>
         </Router>
     );
